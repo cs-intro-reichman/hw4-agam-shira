ff9730b270882166ef2fcee680cfde936049a5c6
Agam Yerucham <agamyerucham@Agams-MacBook-Pro.local>
2024-12-05 00:11:04 +0200

    fixed mystring

--- MyString.java
+++ MyString.java
@@ -35,7 +35,7 @@ public class MyString {
     public static boolean contains(String str1, String str2) {
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
-        if (str1.length() > str2.length()) {
+        if (str2.length() > str1.length()) {
             return false;
         } 
         if (str2.isEmpty()) {
